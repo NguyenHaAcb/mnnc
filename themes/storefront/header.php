b636248b19860c009693babec80569c0f577bdb0
ngyenthiha21092k3@gmail.com <ngyenthiha21092k3@gmail.com>
2023-05-22 22:00:12 +0700

    first commit

---
+++ themes/storefront/header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package storefront
+ */
+
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<?php do_action( 'storefront_before_site' ); ?>
+
+<div id="page" class="hfeed site">
+	<?php do_action( 'storefront_before_header' ); ?>
+
+	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+
+		<?php
+		
+		do_action( 'storefront_header' );
+		?>
+
+	</header><!-- #masthead -->
+
+	<?php
+	/**
+	 * Functions hooked in to storefront_before_content
+	 *
+	 * @hooked storefront_header_widget_region - 10
+	 * @hooked woocommerce_breadcrumb - 10
+	 */
+	do_action( 'storefront_before_content' );
+	?>
+
+	<div id="content" class="site-content" tabindex="-1">
+		<div class="col-full">
+
+		<?php
+		do_action( 'storefront_content_top' );
